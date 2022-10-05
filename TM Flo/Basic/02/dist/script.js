773f7403960867f4d2869dc4233af520ccff3688
Florian Kürsten <f.kuersten@hotmail.com>
2022-10-05 11:15:01 +0200

    basic 02 - done - output console

---
+++ TM Flo/Basic/02/dist/script.js
@@ -0,0 +1,6 @@
+var fName = "Florian";
+var IName = "Kürsten";
+var loopTime = 11;
+for (var i = 0; i < loopTime; i++) {
+    console.log("Iteration is " + i + " The Name is " + fName + " ... " + IName);
+}
---
+++ TM Flo/Basic/02/script.ts
@@ -0,0 +1,8 @@
+let fName = "Florian"
+let IName = "Kürsten"
+
+const loopTime = 11;
+
+for (let i = 0; i < loopTime; i++) {
+    console.log(`Iteration is ${i} The Name is ${fName} ... ${IName}`);
+}
