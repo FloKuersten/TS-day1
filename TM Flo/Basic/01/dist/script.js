74436089ffc9169fa16c162d568ca012ff714af6
Florian Kürsten <f.kuersten@hotmail.com>
2022-10-05 10:53:38 +0200

    basic 1 working only console

--- TM Flo/Basic/01/dist/script.js
+++ TM Flo/Basic/01/dist/script.js
@@ -1,11 +1,10 @@
 // program to generate a multiplication table
 // take input from the user
-var number = parseInt(prompt('Enter a number to multiply with: '));
+var number = parseInt(prompt('What multiplication table would you like to see enter a number: '));
 //creating a multiplication table
 for (var i = 1; i <= 10; i++) {
     // multiply i with number
     var result = i * number;
     // display the result
     console.log(number + " * " + i + " = " + result);
-
 }
--- TM Flo/Basic/01/script.ts
+++ TM Flo/Basic/01/script.ts
@@ -1,7 +1,7 @@
 // program to generate a multiplication table
 
 // take input from the user
-const number: number= parseInt(prompt('Enter a number to multiply with: '));
+const number: number= parseInt(prompt('What multiplication table would you like to see enter a number: '));
 
 //creating a multiplication table
 for(let i = 1; i <= 10; i++) {
@@ -11,5 +11,4 @@ for(let i = 1; i <= 10; i++) {
 
     // display the result
     console.log(`${number} * ${i} = ${result}`);
-    document.write(`${number} * ${i} = ${result}`);
 }
